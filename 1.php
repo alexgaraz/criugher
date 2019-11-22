05e2e7fa90caf85494b1b8ea4eed4f00f95ca199
alexgaraz <criugher.g@gmail.com>
2019-11-22 19:04:01 +0200

    books

---
+++ 1.php
@@ -0,0 +1,24 @@
+<?php
+//$x = false;
+//$a = (
+  //  (2+3)==5 &&
+    //(true==-25) ||
+   // ($x ==true));
+    //echo $a;
+
+   // $i = 25;
+    //if($i==1) echo "hello";
+    //else if ($i=2) echo "hello world";
+    //else if ($i==25) echo "hello world and goodbye";
+    //else "error";
+//$x = 10;
+//if($x==25); echo "x este 25";
+
+for ($i=0;$i<10;$i++) {
+    if($i>5) continue; else $i--;
+    echo $i;
+}
+
+
+
+?>
